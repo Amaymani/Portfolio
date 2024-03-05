9f51dac038d9a6b30a21e4a3463a559ed02a6c70
Amaymani <amaytripathi19@gmail.com>
2024-03-05 18:12:59 +0530

    apos fix

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -30,7 +30,7 @@ const AboutMe = () => {
 
             <div className="card pt-14 leading-normal text-xl max-w-[1200px] min-h-[350px] flex flex-col justify-between h-[300px] p-[35px] border-[1px] border-[rgba(255,255,255,.25)] rounded-[20px] bg-[rgba(76,76,76,0.59)] dark:bg-[rgba(0,0,0,0.59)] [box-shadow:0_0_10px_1px_rgba(0,_0,_0,_0.25)] backdrop-filter backdrop-blur-[15px]">
                 <p>
-Welcome to <strong> PARA-Space!</strong> <br/> I'm Amay, a versatile full-stack web developer deeply immersed in the realms of Web 3.0. <br/> From crafting sleek user interfaces to robust back-end functionalities, I bring a holistic approach to web development with the latest tech in the market. Dive into my portfolio and witness the synergy of design and technology as we navigate the exciting future of the digital landscape.</p>
+Welcome to <strong> PARA-Space!</strong> <br/> I&#39;m Amay, a versatile full-stack web developer deeply immersed in the realms of Web 3.0. <br/> From crafting sleek user interfaces to robust back-end functionalities, I bring a holistic approach to web development with the latest tech in the market. Dive into my portfolio and witness the synergy of design and technology as we navigate the exciting future of the digital landscape.</p>
                 <p className="text-sm dark:text-[#dfb25e]">- Amay Mani Tripathi</p>
             </div>
         </div>
